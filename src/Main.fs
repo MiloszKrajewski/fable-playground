a20e85e28a6213f68fbd2c236ae52c2b23d54dfb
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-08-27 00:28:49 +0100

    added watch.py

--- src/Main.fs
+++ src/Main.fs
@@ -13,14 +13,7 @@ module Main =
         Helpers ? give2() :?> int 
         |> Module.multiply
 
-    let private range n = seq {
-        let mutable i = 0
-        while i < n do 
-            yield i
-            i <- i + 1
-    }
-
     let main () = 
-        let message = (range 5) |> Seq.sum |> mul2 |> sprintf "Hello from Fable (2 * (0+1+2+3+4) = %d)"
+        let message = [ 0..4 ] |> Seq.sum |> mul2 |> sprintf "Hello from Fable (2 * (0+1+2+3+4) = %d)"
         let element = Browser.document.getElementById "main"
         element.innerText <- message
